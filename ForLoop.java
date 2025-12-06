c2bab7158daea76a21807107c111b97826804ae0
akandeisaac021-design <akandeisaac021@gmail.com>
2025-12-06 14:41:50 +0100

    TechTribe.java

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop{
+
+public static void main(String []args){
+
+int sum = 0;
+int number =0;
+
+for(;number<=1000;number +=1){
+sum+=number;
+}
+System.out.println(sum);
+}
+}
---
+++ SummationToAThousand.java
@@ -0,0 +1,12 @@
+public class SummationToAThousand{
+
+public static void main(String []args){
+
+int sum = 0;
+
+for(int number =0;number<=1000;number +=1){
+sum+=number;
+}
+System.out.print(sum);
+}
+}
