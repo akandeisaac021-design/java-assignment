8e67fc3ae588aefd2d4b5fa1b2ec595d12e61698
akandeisaac021-design <akandeisaac021@gmail.com>
2026-03-18 10:09:35 +0100

    Update StudentgradeMain.java

--- ObjectOrientedStudentGrade.java/StudentgradeMain.java
+++ ObjectOrientedStudentGrade.java/StudentgradeMain.java
@@ -1,9 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class StudentGradeOOP {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
